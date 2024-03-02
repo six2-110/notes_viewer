787958139637675cabc6e034342d816bd55db085
six2-110 <108448719+six2-110@users.noreply.github.com>
2024-03-02 22:20:40 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const $viewer = document.getElementById('viewer');
 const hide_classes = ['hideR', 'hideG'];
 let spans_hide = {};
 let if_spans_show = {};
-let view_html;
-let _spans_hide, _if_spans_show, notes_data;
+let view_html, notes_data, note_data;
 
 async function load_file(url, type) { // ファイルを読み込む
     try {
@@ -28,7 +27,6 @@ function handle_click_spans(e) {
 
     // 押されたspanの要素はspans_hideのどこに入っているかを検索
     hide_classes.forEach(col => {
-        console.log(spans_hide[col])
         var arr_spans = Array.from(spans_hide[col])
         if (arr_spans.includes(elm)) {
             elm_col = col; // 色
@@ -42,21 +40,29 @@ window.onload = async () => {
     view_html = 'loading...'
 
     view_html = await load_file(`./notes/${url_search}.html`, 'text'); // クエリー文字列からテキストファイルを取得
-    view_html = view_html.replaceAll('  ', '<br>') // 空白を2つ繋げたら改行
+    // view_html = view_html.replaceAll('  ', '<br>') // 空白を2つ繋げたら改行
     $viewer.innerHTML = view_html; // div#viewer にhtmlを反映させる
 
     notes_data = await load_file('data.json', 'json'); // notesのデータをjsonで取得
+    var path = url_search.split('/')
+    var foo = {notes: notes_data};
+    for (let i = 0; i < path.length-1; i++) {
+        foo = foo.notes.find((elm) => elm.folder_name == path[i]);
+    }
+    note_data = foo.notes.find((note) => note.path == url_search);
+
+    document.getElementById('url').innerText = `${note_data.title} (path:${note_data.path})`; // noteのurlを表示
+    document.getElementsByTagName('title')[0].innerText = `${note_data.title} | Notes Viewer`;
 
     hide_classes.forEach(className => {
         spans_hide[className] = document.getElementsByClassName(className); // 隠すspanのHTML要素を色別に取得
-        if_spans_show[className] = Array(spans_hide[className].length).fill(false) // 全部隠すと設定
+        if_spans_show[className] = Array(spans_hide[className].length).fill(false); // 全部隠すと設定
     })
 
     // spanがclickされたときの処理を設定
     hide_classes.forEach(col => {
         for (let i = 0; i < spans_hide[col].length; i++) {
-            console.log(spans_hide[col][i])
-            spans_hide[col][i].addEventListener('click', handle_click_spans)
+            spans_hide[col][i].addEventListener('click', handle_click_spans);
         }
     })
 
@@ -68,26 +74,26 @@ window.onload = async () => {
         hide_classes.forEach(col => {for (i=0; i<spans_hide[col].length; i++) if_spans_show[col][i] = true})
     });
     document.getElementById('hide_hideR').addEventListener('click', () => { // 赤を非表示
-        for (i=0; i<spans_hide.hideR.length; i++) if_spans_show.hideR[i] = false
+        for (i=0; i<spans_hide.hideR.length; i++) if_spans_show.hideR[i] = false;
     });
     document.getElementById('show_hideR').addEventListener('click', () => { // 赤を表示
-        for (i=0; i<spans_hide.hideR.length; i++) if_spans_show.hideR[i] = true
+        for (i=0; i<spans_hide.hideR.length; i++) if_spans_show.hideR[i] = true;
     });
     document.getElementById('hide_hideG').addEventListener('click', () => { // 緑を非表示
-        for (i=0; i<spans_hide.hideG.length; i++) if_spans_show.hideG[i] = false
+        for (i=0; i<spans_hide.hideG.length; i++) if_spans_show.hideG[i] = false;
     });
     document.getElementById('show_hideG').addEventListener('click', () => { // 緑を表示
-        for (i=0; i<spans_hide.hideG.length; i++) if_spans_show.hideG[i] = true
+        for (i=0; i<spans_hide.hideG.length; i++) if_spans_show.hideG[i] = true;
     });
+
+    setInterval(control_spans, 16)
 }
 
-// loop
-setInterval(() => {
-    // spanを隠したり表示したりするようにclassを設定する
+// spanを隠したり表示したりするようにclassを設定する
+function control_spans() {
     hide_classes.forEach(col => {
         for (let i = 0; i < if_spans_show[col].length; i++) {
-            // console.log(spans_hide, if_spans_show[col], i, spans_hide[col][i])
-            elm_clsLi = spans_hide[col][i].classList
+            elm_clsLi = spans_hide[col][i].classList;
 
             if (if_spans_show[col][i]) { // 表示する設定
                 elm_clsLi.add('show');
@@ -96,6 +102,4 @@ setInterval(() => {
             }
         }
     })
-    document.getElementById('url').innerText = url_search; // noteのurlを表示
-    document.getElementsByTagName('title')[0].innerText = `${url_search} | Notes Viewer`;
-}, 16)
+}
